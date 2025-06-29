4fc9340d2afb38813e278cf1c4de213801bd467e
yordansolis <yordansolis2@gmail.com>
2025-06-29 13:20:39 -0500

    Sintaxis basica

---
+++ 01-sintaxis/HolaMundo.java
@@ -0,0 +1,43 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+
+        System.out.println("-------------------");
+
+        System.out.println("Hola mundo");
+        System.out.print("Holaa mundo 2");
+
+        // int es un tipo de variable
+        int numeroUno = 10;
+        int numeroDos = 40;
+
+        int sumarNumeros = numeroUno + numeroDos;
+        System.out.println(sumarNumeros);
+
+        // double es un tipo de variable
+        double numeroUnoDouble = 10.0;
+        double numeroDosDouble = 40.0;
+
+        double sumarNumerosDouble = numeroUnoDouble + numeroDosDouble;
+
+        System.out.println("El resultado es: " + sumarNumerosDouble);
+
+        // Usar char para representar letras, símbolos o cualquier carácter único.
+        char letra = 'A';
+        char simbolo = '$';
+        char simbolo2 = '>';
+        System.out.println(letra);
+        System.out.println(simbolo);
+        System.out.println(simbolo2);
+
+        // String es un tipo de variable
+        String userEmail = "JhordanSolis@gmail.com";
+        System.out.println(userEmail);
+
+        // Boolean
+        boolean verdadero = true;
+        boolean falso = false;
+        System.out.println(verdadero);
+        System.out.println(falso);
+
+    }
+}
